73d11643366e4c1d0c8c8769c147defbcbe751fb
Julieta Rodriguez <104692472+Mariajulieta2003@users.noreply.github.com>
2024-05-13 18:40:13 -0300

    script.js

--- 3-html-css-javascript-calculadora/script.js
+++ 3-html-css-javascript-calculadora/script.js
@@ -1,1 +1,43 @@
 
+
+window.addEventListener('load' , () => {
+  const resultInput = document.querySelector('#result');
+  const additionBtn = document.querySelector('#addition');
+  const substractionBtn = document.querySelector('#substraction');
+  const divisionBtn = document.querySelector('#division');
+  const multiplicacionBtn = document.querySelector('#multiplicacion');
+  const clearBtn = document.querySelector('#clear');
+  const processBtn = document.querySelector('#process');
+  console.log(resultInput)
+
+
+  const addToResult = (value) => {
+    resultInput.value += value;
+  }
+  
+  const handlenumber = (number) => {
+    addToResult(number);
+  }
+  
+  const buttons = document.querySelectorAll('.number');
+
+  buttons.forEach(Btn => btn.addEventListener('click', () => handleNumber(btn.textContent)));
+
+  processBtn.addEventListener('click', () => {
+    resultInput.vale = eval(resultInput.value);
+  });
+
+  additionBtn.addEventListener('click, () => {
+    addToResult('+');
+  });
+
+  substractionBtn.addEventListener('click, () => {
+      addToResult('-');
+  });
+  multiplicationBtn.addEventListener('click, () => {
+      addToResult('*');
+  });
+  divisionBtn.addEventListener('click, () => {
+    addToResult('/');
+  });                            
+});
